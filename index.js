7da9b74e798f62dd91ea8f1dd34e9bb74e87296a
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-12-29 18:21:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middle wares
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connect
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j6uwcgb.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {}
+run().catch((error) => console.error("Database connection error", error));
+
+app.get("/", (req, res) => {
+  res.send("Task App server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Task app server is running on port ${port}`);
+});
